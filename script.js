e535644ba536aca619bb6fb5ec2be90180c8ff91
IdeYou <desenvolvimento.ideyou@gmail.com>
2026-05-22 21:12:40 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleTheme() {
+  document.body.classList.toggle("amanhecer");
+}
+
+function abrirPagina(url) {
+    window.open(url, '_blank');
+}
+
+function toggleTheme() {
+    document.body.classList.toggle("dark-mode");
+}
